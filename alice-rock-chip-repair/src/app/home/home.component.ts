b5eaa4e33e49235253044009f5ec7661be9f8386
sydkaka <sydkaka@icloud.com>
2025-02-17 18:08:59 -0800

    added reviews

--- alice-rock-chip-repair/src/app/home/home.component.ts
+++ alice-rock-chip-repair/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  ngOnInit(): void {
+    this.loadScript('https://static.elfsight.com/platform/platform.js');
+  }
+
+  loadScript(src: string): void {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = true;
+    document.body.appendChild(script);
+  }
 
 }
